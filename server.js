6541d7c12f5f55d076d43912ed712ab67c3f9063
Andrew Hansen <arahan@uw.edu>
2015-05-12 23:29:25 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
-/*var mongo = require('mongodb');
-
-
-
-mongoDb.connect(mongoUri, function(err, db) {
-	db.collection('groupList', function(err, collection) {
-		collection
-	}
-})*/
 
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
 
 var mongojs = require('mongojs');
@@ -20,7 +12,7 @@ var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
 
-
+app.use(cookieParser());
 app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.json());
 
@@ -28,24 +20,44 @@ app.use(bodyParser.json());
 
 
 app.get('/groupList', function(req, res) {
-	console.log('Received GET request');
+	
 	db.groupList.find(function(err, docs) {
 		console.log(err);
 		res.json(docs);
 	});
+	
+});
 
 app.get('/groupList/:id', function(req, res) {
 	console.log('GET ' + req.params.id);
-})
-	
+
+});
+
+app.get('/userCreated/:id', function(req, res) {
+	console.log(req.cookies.userCreated);
+	if (req.cookies.userCreated == req.params.id) {
+		res.send(true);
+	}
+	else {
+		res.send(false);
+	}
 	
 });
 
 app.post('/groupList', function(req, res) {
-	console.log('POSTed data');
+	
 	req.body._id = mongojs.ObjectId();
 	db.groupList.insert(req.body, function(err, doc) {
-		
+		if (err) {
+			console.log(err);
+		}
+
+		try {
+			res.cookie('userCreated', doc._id);
+		}
+		catch (e) {
+			console.log(e);
+		}
 		res.json(doc);
 		
 	});
